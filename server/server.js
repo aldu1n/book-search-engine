2477b858d62f7582aab9df6c8923be116f5df8e0
Bogdan Tkachuk <cnndo14@gmail.com>
2024-03-06 15:54:48 -0500

    added apolloserver to server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,52 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-
 const app = express();
+
+// ApolloServer imports.
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+
+// GraphQL imports.
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Create a new instance of an Apollo server with the GraphQL schema.
+const startApolloServer = async () => {
+  await server.start();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-app.use(routes);
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  // If we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(_dirname, '../client/dist/index.html'));
+    });
+  };
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+// Call the function to start the server.
+startApolloServer();
+
